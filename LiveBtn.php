e57f27bb7fecf9403beba61e571f19401570fe3e
Jay Greentree <JayGreentree@users.noreply.github.com>
2022-11-28 18:17:36 -0500

    Create LiveBtn.php

---
+++ LiveBtn.php
@@ -0,0 +1,4 @@
+<?php if ((date("w") == 0) && (date("Hi")>1030) && (date("Hi")<1200)) {?>
+
+    <span class="toolbar"><a href="//domain.com/live" class="btn btn_transparent">Link</a></span>
+<?php } ?>
